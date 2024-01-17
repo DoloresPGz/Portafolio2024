07b02a1c108d8f407428ae1423d86b2254733e7f
DoloresPGz <dolores.pgz@gmail.com>
2024-01-16 18:05:14 -0600

    update of components of agenda

---
+++ agenda-tareas/src/components/TaskDetail/TaskDetail.jsx
@@ -0,0 +1,16 @@
+const TaskDetail = ({ handleDone }) => {
+  return (
+    <div className=''>
+      <h1 className=''>Mi día</h1>
+      <h2 className=''>Tareas pendietes</h2>
+      <ul>
+        <li>Lavar ropa</li>
+      </ul>
+      <h2 className=''>Tareas terminadas</h2>
+      <ul>
+        <li>Salir a caminar</li>
+      </ul>
+    </div>
+  )
+}
+export default TaskDetail
